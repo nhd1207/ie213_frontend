0dbee2cbc98116bfd5e54a679b6861e2cc58e6ec
jgooboi <19520467@gm.uit.edu.vn>
2022-04-04 15:37:36 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import AdminPage from "./screens/AdminPage/AdminPage";
 import LayoutAdmin from "./components/Admin/LayoutAdmin/LayoutAdmin";
 import CarTable from "./components/Admin/CarTable/CarTable";
+import Showroom from "./screens/ShowroomPage/Showroom";
 
 function App() {
   return (
     <div className="App">
      {/* <LayoutAdmin></LayoutAdmin> */}
-     <CarTable></CarTable>
+     <Showroom></Showroom>
     </div>
   );
 }
