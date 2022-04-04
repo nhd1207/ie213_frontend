65f4cf5d23c1c708fceffe60f6ef4c5529b0c78c
jgooboi <19520467@gm.uit.edu.vn>
2022-04-04 16:23:03 +0700

    shadow

--- src/screens/ShowroomPage/Showroom.js
+++ src/screens/ShowroomPage/Showroom.js
@@ -4,9 +4,9 @@ export default function Showroom() {
   return (
     <div className="SHcon">
         <div className="Banner">
-          <h2 className="h2-top">CHUỖI</h2>
-          <h1>SHOWROOMS</h1>
-          <h2 className="h2-bot">LỚN NHẤT CẢ NƯỚC</h2>
+          <h2 className="title">CHUỖI</h2>
+          <h1 className="title">SHOWROOMS</h1>
+          <h2  className="title">LỚN NHẤT CẢ NƯỚC</h2>
         </div>
         
     </div>
