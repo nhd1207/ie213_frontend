c2b853ed037fb3a606446b21d553d7e995d4c4f6
jgooboi <19520467@gm.uit.edu.vn>
2022-04-04 16:06:42 +0700

    banner

--- src/screens/ShowroomPage/Showroom.js
+++ src/screens/ShowroomPage/Showroom.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import './Showroom.css'
 export default function Showroom() {
   return (
-    <div>Showroom</div>
+    <div className="SHcon">
+        <div className="Banner">
+          <h2 className="h2-top">CHUỖI</h2>
+          <h1>SHOWROOMS</h1>
+          <h2 className="h2-bot">LỚN NHẤT CẢ NƯỚC</h2>
+        </div>
+        
+    </div>
   )
 }
