ccead2046fb5549620ef39b9b83c8fe7390ff5a2
Hiệp Nguyễn Hoàng <19520536@gm.uit.edu.vn>
2022-04-07 17:19:30 +0700

    Update RendertInputGroup.js

--- src/components/Share/RendertInputGroup.js
+++ src/components/Share/RendertInputGroup.js
@@ -11,18 +11,26 @@ import {
     Tooltip,
   } from 'antd';
   import { CopyOutlined } from '@ant-design/icons';
+  import React from 'react'
   
   const { Option } = Select;
 
 
-<Input.Group compact>
-      <Select defaultValue="Sign Up" style={{ width: '30%' }}>
-        <Option value="Sign Up">Sign Up</Option>
-        <Option value="Sign In">Sign In</Option>
-      </Select>
-      <AutoComplete
-        style={{ width: '70%' }}
-        placeholder="Email"
-        options={[{ value: 'text 1' }, { value: 'text 2' }]}
-      />
-    </Input.Group>
+  
+  export default function RendertInputGroup() {
+    return (
+        <Input.Group compact>
+        <Select defaultValue="Sign Up" style={{ width: '30%' }}>
+          <Option value="Sign Up">Sign Up</Option>
+          <Option value="Sign In">Sign In</Option>
+        </Select>
+        <AutoComplete
+          style={{ width: '70%' }}
+          placeholder="Email"
+          options={[{ value: 'text 1' }, { value: 'text 2' }]}
+        />
+      </Input.Group>
+    )
+  }
+  
+
