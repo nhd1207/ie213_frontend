fbf044b4de5b5cc2b3feaca7189b6674c92b2c6d
jgooboi <19520467@gm.uit.edu.vn>
2022-04-07 16:36:12 +0700

    triangle
    luong ngu vcl

--- src/screens/ShowroomPage/Showroom.js
+++ src/screens/ShowroomPage/Showroom.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import './Showroom.css'
+import style from './Showroom.module.css'
 export default function Showroom() {
   return (
-    <div className="SHcon">
-        <div className="Banner">
-          <h2 className="title">CHUỖI</h2>
-          <h1 className="title">SHOWROOMS</h1>
-          <h2  className="title">LỚN NHẤT CẢ NƯỚC</h2>
+    <div className={style.SHcon}>
+        <div className={style.Banner}>
+          <h2 className={style.title}>CHUỖI</h2>
+          <h1 className={style.title}>SHOWROOMS</h1>
+          <h2  className={style.title}>LỚN NHẤT CẢ NƯỚC</h2>
         </div>
-        <div className="HCM-space">
-            <h1 className="city-title">HỒ CHÍ MINH</h1>
-            <div className="HCM-space-content">Đây là 1 đoạn giới thiệu,  Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu,  Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu<br/><br/>
+        <div className={style.HCM_space}>
+            <h1 className={style.city_title}>HỒ CHÍ MINH</h1>
+            <div className={style.HCM_space_content}>Đây là 1 đoạn giới thiệu,  Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu,  Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu<br/><br/>
                  Đây là 1 đoạn giới thiệu,  Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu,  Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu<br/></div>
         </div>
     </div>
