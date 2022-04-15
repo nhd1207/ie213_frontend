95fe25166e360f2409563b29bf25abdbfeec2413
luong228 <90311663+luong228@users.noreply.github.com>
2022-04-15 16:21:44 +0700

    Create Showroom.js

---
+++ src/apis/Showroom.js
@@ -0,0 +1,43 @@
+import axios from '../requestV2'
+
+const prefix = '/showRoom';
+
+export const getDetailByCode =(code)=> {
+    return axios({
+        method: 'GET',
+        url: `${prefix}/${code}`
+    })
+}
+
+export const getList =()=> {
+    return axios({
+        method: 'GET',
+        url: `${prefix}/`
+    })
+}
+
+export const create =(data)=> {
+    return axios({
+        method: 'POST',
+        data,
+        url: `${prefix}/`
+    })
+}
+
+export const update =(id, data)=> {
+    return axios({
+        method: 'PUT',
+        data,
+        url: `${prefix}/${id}`
+    })
+}
+
+export const destroy =(id)=> {
+    return axios({
+        method: 'DELETE',
+        url: `${prefix}/${id}`
+    })
+}
+
+
+
