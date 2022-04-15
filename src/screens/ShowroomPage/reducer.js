6294ea4ec1e0e0f243d3b73f3474909c7f453ec0
luong228 <90311663+luong228@users.noreply.github.com>
2022-04-15 17:05:45 +0700

    Update reducer.js

--- src/screens/ShowroomPage/reducer.js
+++ src/screens/ShowroomPage/reducer.js
@@ -1,61 +1,49 @@
-import { action_type as type } from "./action";
+import { action_type as type } from './action'
 
 const initialState = {
-  loading: false,
-  user: {},
-};
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case type.LOGIN.REQUEST:
-      return {
-        ...state,
-        loading: true,
-      };
-    case type.LOGIN.SUCCESS:
-      return {
-        ...state,
-        loading: false,
-      };
-    case type.LOGIN.ERROR:
-      return {
-        ...state,
-        loading: false,
-      };
-    case type.VERIFY.REQUEST:
-      return {
-        ...state,
-        loading: true,
-      };
-    case type.VERIFY.SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        user: action?.data || {},
-      };
-    case type.VERIFY.ERROR:
-      return {
-        ...state,
-        loading: false,
-      };
-    case type.USER.REQUEST:
-      return {
-        ...state,
-        loading: true,
-      };
-    case type.USER.SUCCESS:
-      return {
-        ...state,
-        loading: false,
-      };
-    case type.USER.ERROR:
-      return {
-        ...state,
-        loading: false,
-      };
-    default:
-      return state;
-  }
+    loading: false,
+    data: [],
+    posts: [],
+    cars: []
 }
 
-export default reducer;
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case type.GETCAR.REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case type.GETCAR.SUCCESS:
+            return {
+                ...state,
+                cars: action.data,
+                loading: false,
+            }
+        case type.GETCAR.ERROR:
+            return {
+                ...state,
+                loading: false,
+            }
+        case type.GETPOST.REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case type.GETPOST.SUCCESS:
+            return {
+                ...state,
+                posts: action.data,
+                loading: false,
+            }
+        case type.GETPOST.ERROR:
+            return {
+                ...state,
+                loading: false,
+            }
+        default:
+            return state
+    }
+}
+
+export default reducer
