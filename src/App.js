eef4dcaf91c2927a6eab37242e68a70b05e1c8db
LCD1502 <skillpb123@gmail.com>
2022-05-03 18:37:19 +0700

    cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import LoginForm from "./components/Authentication/Login/LoginForm";
-import CarOvierview from "./components/CarDetail/CarOvierview";
 import "./App.css";
-import CarList from "./components/CarList/CarList";
+import WishList from "./components/wishList/WishList";
 
 function App() {
   return (
     <div className="App">
-      <CarList></CarList>
+      <WishList></WishList>
     </div>
   );
 }
---
+++ src/components/wishList/WishList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import style from "./WishList.module.css";
+import "antd/dist/antd.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import {
+  TagsOutlined,
+  CarOutlined,
+  PlusCircleOutlined,
+  MinusCircleOutlined,
+  CheckCircleOutlined,
+  PlusSquareOutlined,
+  RightCircleOutlined,
+} from "@ant-design/icons";
+
+export default function WishList() {
+  return (
+    <div>
+      <h1>Wish List</h1>
+      <h2>Wish List</h2>
+    </div>
+  );
+}
