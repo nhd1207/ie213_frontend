a629cdda590428ecd912417e4eb13d5df5ee42c2
LCD1502 <skillpb123@gmail.com>
2022-05-09 09:10:57 +0700

    support page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import CarDetail from "./components/CarDetail/CarDetail";
 import CompareTwoCar from "./components/CompareTwoCar/CompareTwoCar";
 import AccessoryDetail from "./components/AccessoryDetail/AccessoryDetail";
 import AboutUs from "./components/AboutUs/AboutUs";
+import Support from "./components/Support/Support";
 
 function App() {
   return (
     <div className="App">
-      <AboutUs></AboutUs>
+      <Support></Support>
     </div>
   );
 }
