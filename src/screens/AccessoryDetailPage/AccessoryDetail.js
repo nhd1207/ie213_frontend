f813fabe33c9ad9134a988a8d19e92704b90dc40
luong228 <90311663+luong228@users.noreply.github.com>
2022-05-20 18:33:49 +0700

    add heart icon on accessory detail

--- src/screens/AccessoryDetailPage/AccessoryDetail.js
+++ src/screens/AccessoryDetailPage/AccessoryDetail.js
@@ -13,6 +13,7 @@ import {
     Dropdown,
     CarouselItem,
 } from "react-bootstrap";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Spin, Carousel } from "antd";
 import money from '../../components/Share/functions/money'
@@ -129,11 +130,14 @@ function AccessoryDetail(props) {
                                 {/* <div className={`${style.document} col-xl-3`}>Thông số: {props.data?.data[0]?.specification?.height} cm, {props.data?.data[0]?.specification.weight} kg</div>
                                 <div className={`${style.content_price} ${style.document} col-xl-3`}>
                                     Giá tiền: {money(props.data?.data[0]?.price, "VND")} */}
-                                <div className={`${style.FromUpdateCart}`}>
-                                    <FormUpdateCart
-                                        onSubmit={handleUpdateCart}
-                                        colors={props.data?.data[0]?.color}
-                                    />
+                                <div className={`${style.btnWrapper}`}>
+                                    <div className={`${style.FromUpdateCart}`}>
+                                        <FormUpdateCart
+                                            onSubmit={handleUpdateCart}
+                                            colors={props.data?.data[0]?.color}
+                                        />
+                                    </div>
+                                    
                                 </div>
                             </div>
 
