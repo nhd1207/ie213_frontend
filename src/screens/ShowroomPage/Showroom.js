9b4c51e4be992e86794ff3518bf6264bd9f16d4c
Hiệp Nguyễn Hoàng <19520536@gm.uit.edu.vn>
2022-06-03 15:21:16 +0700

    update showroom and cart

--- src/screens/ShowroomPage/Showroom.js
+++ src/screens/ShowroomPage/Showroom.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 import style from "./showroom.module.css";
-import Showroom2 from "../../Images/Showroom2.png";
-import Showroom3 from "../../Images/Showroom3.png";
 import { getListShowroom } from "./action";
 import { connect } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,14 +7,12 @@ import Layout from "../../components/layout";
 import {Spin} from "antd"
 function Showroom(props) {
   useEffect(() => {
-    // let params = {};
     props.getListShowroom();
     console.log("props", props);
   }, []);
   return (
     <Layout>
       <Spin spinning={props.loading}>
-
       <div className={`${style.SHcon} `}>
         <div className={`${style.Banner}`}>
           <h2 className={`${style.title}`}>CHUỖI</h2>
@@ -28,70 +24,49 @@ function Showroom(props) {
             <hr className={`${style.line} col-xl-12`}></hr>
             <h1 className={`${style.city_title} col-xl-12`}>HỒ CHÍ MINH</h1>
             <div className={`${style.leftContent__desc} col-xl-12`}>
-              Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn
-              giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu,
-              Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn
-              giới thiệu
-              <br />
-              <br />
-              Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn
-              giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu,
-              Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn
-              giới thiệu
+            Thành phố Hồ Chí Minh, còn gọi bằng tên cũ phổ biến là Sài Gòn, 
+            là thành phố lớn nhất ở Việt Nam về dân số và quy mô đô thị hóa. 
+              <br/><br/>
+            Thành phố Hồ Chí Minh là một trong những điểm nóng về xe hơi, vì thế chúng tôi mang
+            đến cho bạn những showroom tuyệt vời nhất cho trải nghiệm của bạn
               <br />
             </div>
             <div className={`${style.leftContent__desc} col-xl-12`}>
-              
             {props?.showrooms?.filter(item=>  item.address.includes('Hồ Chí Minh')).map(item=>{
               return <>
               <hr className={`${style.line}`}></hr> 
               {item.name}: {item.address}
               </>
             })}
-             
-              {/* {props.showRoom?.adress[0]}
-              <hr className={`${style.line}`}></hr>
-              123, đường 567, Q1, Tp.HCM
-              <hr className={`${style.line}`}></hr> */}
             </div>
           </div>
           <div
             className={`${style.leftContent__img_wrapper} col-xl-7 col-md-12`}
           >
             <div className={`${style.img_wrapper_1} col-xl-12`}>
-              {/* <img src={Showroom2}></img> */}
             </div>
             <div className={`${style.img_wrapper_2} col-xl-12`}>
-              {/* <img src={Showroom3}></img> */}
             </div>
           </div>
         </div>
-
         <div className={`${style.rightContent__wrapper} row`}>
           <div
             className={`${style.rightContent__image_wrapper} col-xl-7 col-md-12 row`}
           >
             <div className={`${style.img_wrapper_3} col-xl-12 `}>
-              {/* <img src={Showroom2}></img> */}
             </div>
             <div className={`${style.img_wrapper_4} col-xl-12 `}>
-              {/* <img src={Showroom3}></img> */}
             </div>
           </div>
           <div className={`${style.rightContent__text} col-xl-5 col-md-12 `}>
             <hr className={`${style.line} col-xl-12`}></hr>
             <h1 className={`${style.city_title} col-xl-12 `}>ĐÀ NẴNG</h1>
             <div className={`${style.rightContent__desc} col-xl-12 `}>
-              Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn
-              giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu,
-              Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn
-              giới thiệu
-              <br />
-              <br />
-              Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn
-              giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu,
-              Đây là 1 đoạn giới thiệu, Đây là 1 đoạn giới thiệu, Đây là 1 đoạn
-              giới thiệu
+            Đà Nẵng đã thay đổi chóng mặt với tốc độ phát triển kinh hoàng đó là lời 
+            nhận xét của đa số mọi người khi quay lại nơi đây.
+              <br/><br/>
+            Thành phố Đà Nẵng là một trong những điểm nóng về xe hơi, vì thế chúng tôi mang
+            đến cho bạn những showroom tuyệt vời nhất cho trải nghiệm của bạn
               <br />
             </div>
             <div className={`${style.rightContent__desc} col-xl-12 `}>
