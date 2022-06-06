29f7d4540c5c1da0c30976569e0a72a98bcd9e72
luong228 <90311663+luong228@users.noreply.github.com>
2022-06-06 12:11:56 +0700

    fix % dashboard

--- src/components/Admin/AdminHome/AdminHome.js
+++ src/components/Admin/AdminHome/AdminHome.js
@@ -60,19 +60,16 @@ function AdminHome(props) {
     const COLORSUSER = ["#008000", "#FF0000"];
     const COLORCARORDER = ["#f0a500", "#008000", "#0000FF", "#FF0000"];
     const RADIAN = Math.PI / 180;
-    const renderCustomizedLabel = ({
-        cx,
-        cy,
-        midAngle,
-        innerRadius,
-        outerRadius,
-        percent,
-        index,
-    }) => {
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
         const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
         const x = cx + radius * Math.cos(-midAngle * RADIAN);
         const y = cy + radius * Math.sin(-midAngle * RADIAN);
-    }
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
 
     return (
         <div>
